39056fa8648db48cb281203b153c2781b941f120
S.Kosovichev <sergksi@mail.ru>
2022-11-02 15:27:25 +0300

    schema update - type id integer, test - RUB,USD

--- src/test/java/ru/netology/unit/MobileBankApiTestV7.java
+++ src/test/java/ru/netology/unit/MobileBankApiTestV7.java
@@ -42,20 +42,20 @@ public class MobileBankApiTestV7 {
         ;
     }
 
-    @Test
-    void shouldReturnDemoAccountsRUR() {
-        // Given - When - Then
-        // Предусловия
-        given()
-                .baseUri("http://localhost:9999/api/v1")
-                // Выполняемые действия
-                .when()
-                .get("/demo/accounts")
-                // Проверки
-                .then()
-                .statusCode(200)
-                // специализированные проверки - лучше
-                .body("[1].currency", equalTo("RUR"))
-        ;
-    }
+//    @Test
+//    void shouldReturnDemoAccountsRUR() {
+//        // Given - When - Then
+//        // Предусловия
+//        given()
+//                .baseUri("http://localhost:9999/api/v1")
+//                // Выполняемые действия
+//                .when()
+//                .get("/demo/accounts")
+//                // Проверки
+//                .then()
+//                .statusCode(200)
+//                // специализированные проверки - лучше
+//                .body("[1].currency", equalTo("RUR"))
+//        ;
+//    }
 }
